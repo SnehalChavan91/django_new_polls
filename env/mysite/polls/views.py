5a018ef1bdccae349c2c9bef0e1c4b83fc56cac9
snehal <you@example.com>
2024-02-26 14:09:30 +0530

    loads the template polls/index.html and passed it a context

--- env/mysite/polls/views.py
+++ env/mysite/polls/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Question
+from django.template import loader
 
 def index(request):
     latest_question_list=Question.objects.order_by("-pub_date")[:5]
-    for q in latest_question_list:
-        output=",".join(q.question_text)
-    return HttpResponse(output)
-
+    template=loader.get_template("polls/index.html")
+    context={
+        "latest_question_list":latest_question_list,
+    }
+    return HttpResponse(template.render(context,request))
 def contacts(request):
     return HttpResponse("Hello contacts.")
 
