a739af9912a11e37035e952c07065b1717308bff
snehal <you@example.com>
2024-02-26 13:23:46 +0530

    details,results,vote views defined and urls are set for them

--- env/mysite/polls/views.py
+++ env/mysite/polls/views.py
@@ -6,3 +6,15 @@ def index(request):
 
 def contacts(request):
     return HttpResponse("Hello contacts.")
+
+
+def detail(request,question_id):
+    return HttpResponse("You are looking at question %s"% question_id)
+
+def results(request,question_id):
+    response="You are looking at the result of question %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request,question_id):
+    return HttpResponse("You are voting on question %s"% question_id)
